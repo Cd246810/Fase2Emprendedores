47a79993de5ab088da554b5be3229a621a431a05
Cd246810 <christian246810@gmail.com>
2017-10-25 21:15:24 -0600

    Adios FireBase, y gracias por nada Google XD

---
+++ Prueba3/index.js
@@ -0,0 +1,40 @@
+var express=require("express");
+var session = require("express-session"); //Variabel de sesion
+var bodyParser=require("body-parser"); //Jalar Info del post
+var app=express(); 
+var fs = require('fs'); //Jalar archivos
+var server = require('http').Server(app);  //Se le puedan hacer request .http
+//var io = require('socket.io')(server);   //Sockets
+
+app.use(session({secret: 'ssshhhhh'})); //Sesion secreta
+
+app.use(express.static('public'));  //No me acuero pero parece necesario
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set("view engine","jade"); //Arhcivos Jade como visualizador
+
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/css2', express.static(__dirname + '/Css')); // redirect CSS bootstrap
+
+server.listen(7070, function() {  
+    console.log('Servidor corriendo en http://localhost:7070');
+});
+
+app.get("/",function(req,res){
+	res.render("login");
+});
+
+app.get("/Login",function(req,res){
+	res.render("login");
+});
+
+app.post('/Login', function (req, res) {
+	req.session.user_id = "Admin";//Guardar dato en variable de sesion
+   console.log('Ingreso con: '+ req.body.Nombre + req.body.Contra );
+   res.render("login");
+});
